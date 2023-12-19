ef33355964c99b5cc46134e250516b95736463b6
Eric Gattschau <eric.gattschau@port-zero.com>
2023-12-19 21:26:05 +0100

    update

---
+++ christmas_tree_w_snow.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="git" name="christmas_tree_w_snow" tilewidth="32" tileheight="32" tilecount="40" columns="8">
+ <image source="christmas_tree-1.0/PNG/32x32/christmas_tree_w_snow.png" width="256" height="160"/>
+</tileset>
