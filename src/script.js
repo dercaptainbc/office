e24a07e86d86e00c6ffb70fa2ee0d90e6ea617f3
eric.gattschau <eric.gattschau@port-zero.com>
2025-10-28 14:34:02 +0100

    refactor: simplify exit zone subscription by removing unnecessary variable and unsubscribe call

--- src/script.js
+++ src/script.js
@@ -1,7 +1,4 @@
-const exit_to_frachtwerk = WA.room.area.onEnter("exit_to_frachtwerk").subscribe(() => {
+WA.room.area.onEnter("exit_to_frachtwerk").subscribe(() => {
     console.log("Entering exit zone");
     WA.nav.goToPage("https://workadventure.stetigwandel.de/_/global/workadventure.stetigwandel.de/map-storage/frachtwerk/fw-office.tmj");
 });
-
-
-exit_to_frachtwerk.unsubscribe();
