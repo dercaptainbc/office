bd27df174910ee5a116ce6b7fdbf6c1581d7a929
eric.gattschau <eric.gattschau@port-zero.com>
2025-10-28 13:53:44 +0100

    update script reference in office-halloween map and add exit zone functionality

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 WA.chat.sendChatMessage('Hello world', 'Mr Robot');
+
+// exit-script.ts
+WA.room.onEnterZone("exit_to_frachtwerk", () => {
+  console.log("Entering exit zone");
+  WA.nav.goToPage("https://workadventure.stetigwandel.de/_/global/workadventure.stetigwandel.de/map-storage/frachtwerk/fw-office.tmj");
+});
