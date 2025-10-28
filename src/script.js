305ee1d3b5556e29a6d4e5dde5e2de82f5c491ed
eric.gattschau <eric.gattschau@port-zero.com>
2025-10-28 17:03:59 +0100

    fix: remove unsubscribe from exitToFrachtwerkSubscription on leave

--- src/script.js
+++ src/script.js
@@ -9,6 +9,5 @@ const exitToFrachtwerkSubcription = WA.room.onEnterLayer("exit_to_frachtwerk").s
 
 WA.room.onLeaveLayer("exit_to_frachtwerk").subscribe(() => {
     WA.chat.sendChatMessage("You have left the exit zone.", { scope: 'local', author: 'Mr Robot' });
-    exitToFrachtwerkSubcription.unsubscribe();
     clearTimeout(exitTimeout);
 });
