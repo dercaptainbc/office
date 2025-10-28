a5961dfdbbe5cb82e29badde5350eae3b06d1ac1
eric.gattschau <eric.gattschau@port-zero.com>
2025-10-28 13:44:56 +0100

    refactor: move exit zone script to a new file and update main script

--- src/main.ts
+++ src/main.ts
@@ -34,9 +34,3 @@ function closePopup(){
 }
 
 export {};
-
-
-WA.room.onEnterZone("exit_to_frachtwerk", () => {
-  console.log("Entering exit zone");
-  WA.nav.goToPage("https://workadventure.stetigwandel.de/_/global/workadventure.stetigwandel.de/map-storage/frachtwerk/fw-office.tmj");
-});
