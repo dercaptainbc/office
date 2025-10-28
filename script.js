a602459c82a7ca7d356a306e49a6a7757445d15d
eric.gattschau <eric.gattschau@port-zero.com>
2025-10-28 15:52:50 +0100

    fix: add missing closing bracket for exit layer leave event

--- script.js
+++ script.js
@@ -5,4 +5,4 @@ const exitToFrachtwerkSubcription = WA.room.onEnterLayer("exit_to_frachtwerk").s
 WA.room.onLeaveLayer("exit_to_frachtwerk").subscribe(() => {
     WA.chat.sendMessage("You have left the exit zone.");
     exitToFrachtwerkSubcription.unsubscribe();
-}
+});
