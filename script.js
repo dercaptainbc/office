5668a52a640d2f3de6e5843ce4725102af82559b
eric.gattschau <eric.gattschau@port-zero.com>
2025-10-28 15:02:45 +0100

    fix: update script path for improved portability and add exit zone functionality

---
+++ script.js
@@ -0,0 +1,4 @@
+WA.room.onEnterLayer("exit_to_frachtwerk").subscribe(() => {
+    console.log("Entering exit zone");
+    WA.nav.goToPage("https://workadventure.stetigwandel.de/_/global/workadventure.stetigwandel.de/map-storage/frachtwerk/fw-office.tmj");
+});
