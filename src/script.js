db8f2601f85f258c42d83e9f7f918a1167f51e52
eric.gattschau <eric.gattschau@port-zero.com>
2025-10-28 16:35:17 +0100

    fix: enhance exit layer messaging and navigation for teleportation to Frachtwerk

--- src/script.js
+++ src/script.js
@@ -1,4 +1,11 @@
-WA.room.onEnterLayer("exit_to_frachtwerk").subscribe(() => {
-    console.log("Entering exit zone");
-    WA.nav.goToPage("https://workadventure.stetigwandel.de/_/global/workadventure.stetigwandel.de/map-storage/frachtwerk/fw-office.tmj");
+const exitToFrachtwerkSubcription = WA.room.onEnterLayer("exit_to_frachtwerk").subscribe(() => {
+    WA.chat.sendChatMessage('Preparing to teleport you to Frachtwerk...', { scope: 'local', author: 'Mr Robot' });
+    setTimeout(() => {
+        WA.nav.goToPage("https://workadventure.stetigwandel.de/_/global/workadventure.stetigwandel.de/map-storage/frachtwerk/fw-office.tmj");
+    }, 2000);
+});
+
+WA.room.onLeaveLayer("exit_to_frachtwerk").subscribe(() => {
+    WA.chat.sendChatMessage("You have left the exit zone.", { scope: 'local', author: 'Mr Robot' });
+    exitToFrachtwerkSubcription.unsubscribe();
 });
