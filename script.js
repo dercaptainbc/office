2d74861a214aa9acd7f1f153d25395ae5d9bf9b7
eric.gattschau <eric.gattschau@port-zero.com>
2025-10-28 15:45:40 +0100

    fix: comment out navigation URL on entering exit zone

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 WA.room.onEnterLayer("exit_to_frachtwerk").subscribe(() => {
     console.log("Entering exit zone");
-    WA.nav.goToPage("https://workadventure.stetigwandel.de");
+    //WA.nav.goToPage("https://workadventure.stetigwandel.de");
+});
+
+WA.room.onLeaveLayer("exit_to_frachtwerk").subscribe(() => {
+    console.log("Leaving exit zone");
 });
