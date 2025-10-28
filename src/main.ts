6c44090ed57d5d51045a7b916127077ae9a6d85f
eric.gattschau <eric.gattschau@port-zero.com>
2025-10-28 13:40:44 +0100

    add exit zone for navigation to frachtwerk

--- src/main.ts
+++ src/main.ts
@@ -34,3 +34,9 @@ function closePopup(){
 }
 
 export {};
+
+
+WA.room.onEnterZone("exit_to_frachtwerk", () => {
+  console.log("Entering exit zone");
+  WA.nav.goToPage("https://workadventure.stetigwandel.de/_/global/workadventure.stetigwandel.de/map-storage/frachtwerk/fw-office.tmj");
+});
